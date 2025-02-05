c51c384fd47726393cefb0a43a8a226ae34237f0
Ming Chi <wong_mingchi@yahoo.co.uk>
2025-02-05 14:37:13 +0000

    Data fetched implemented

--- src/component/store-front/Storefront.tsx
+++ src/component/store-front/Storefront.tsx
@@ -1,13 +1,39 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { Storefrontdata } from "../types/Types";
 
 function Storefront() {
-    const [products, setProducts]=useState()
-    useEffect(()=>{
+  const [products, setProducts] = useState<Storefrontdata[]>([]); // Initialize as an empty array
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError]=useState<boolean>()
 
-    },[])
+  useEffect(() => {
+    const productDataFetch = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    productDataFetch();
+  }, []);
+
   return (
     <div>
       <h1>Products</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        products.map((product) => <p key={product.id}>{product.title}</p>)
+      )}:{
+
+      }
     </div>
   );
 }
---
+++ src/component/types/Types.ts
@@ -0,0 +1,13 @@
+export interface Storefrontdata {
+    category:string;
+    description:string;
+    image:string;
+    price:number;
+    rating:{
+        count:number;
+        rate:number;
+    }
+    title:string;
+    
+
+}
