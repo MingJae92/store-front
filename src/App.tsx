73786cadb7ba465ffc0dac1f0fbaf10f58a28eda
Ming Chi <wong_mingchi@yahoo.co.uk>
2025-02-05 13:25:09 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Shoppingcart from './component/shopping-cart/Shoppingcart'
+import Storefront from './component/store-front/Storefront'
+
+function App() {
+ 
+
+  return (
+    <div>
+     <Storefront/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/store-front/Storefront.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function Storefront() {
+    const [products, setProducts]=useState()
+    useEffect(()=>{
+
+    },[])
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  );
+}
+
+export default Storefront;
